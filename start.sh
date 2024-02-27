ed0c4a57ef050d79fadfedfd23a8a12f12079982
m5kro <44246711+m5kro@users.noreply.github.com>
2024-02-27 00:03:15 -0800

    Use nwjs 0.84.0

--- start.sh
+++ start.sh
@@ -13,15 +13,15 @@ else
     if [ -f ~/.local/share/porter/nwjs/nw ]; then
         echo "nwjs found!"
     else
-        echo "nwjs not found! Downloading version 0.78.1 (this only needs to be done once)"
+        echo "nwjs not found! Downloading version 0.84.0 (latest as of this script)"
         mkdir ~/.local/share/porter
         if command -v curl > /dev/null 2>&1; then
             echo "curl found! Downloading..."
-            curl https://dl.nwjs.io/v0.78.1/nwjs-sdk-v0.78.1-linux-x64.tar.gz -o ~/.local/share/porter/nwjs.tar.gz
+            curl https://dl.nwjs.io/v0.84.0/nwjs-sdk-v0.84.0-linux-x64.tar.gz -o ~/.local/share/porter/nwjs.tar.gz
             tar -xzvf ~/.local/share/porter/nwjs.tar.gz -C ~/.local/share/porter/
-            mv ~/.local/share/porter/nwjs-sdk-v0.78.1-linux-x64 ~/.local/share/porter/nwjs
+            mv ~/.local/share/porter/nwjs-sdk-v0.84.0-linux-x64 ~/.local/share/porter/nwjs
             rm -f ~/.local/share/porter/nwjs.tar.gz
-            echo "0.78.1" > ~/.local/share/porter/nwjs/nwjs-version.txt
+            echo "0.84.0" > ~/.local/share/porter/nwjs/nwjs-version.txt
             curl -L https://github.com/m5kro/cicpoffs/releases/download/binary/cicpoffs -o  ~/.local/share/porter/cicpoffs
             chmod +x ~/.local/share/porter/cicpoffs
         else
